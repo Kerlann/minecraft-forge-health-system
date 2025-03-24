316b764c61e7589b46b6a34f0fa2f85a2c1bbc3d
Kerlann <34661926+Kerlann@users.noreply.github.com>
2025-03-24 21:36:30 +0100

    Mise à jour de ClientProxy.java pour initialiser l'overlay HUD

--- src/main/java/com/kerlann/healthsystem/client/ClientProxy.java
+++ src/main/java/com/kerlann/healthsystem/client/ClientProxy.java
@@ -1,26 +1,40 @@
 package com.kerlann.healthsystem.client;
 
 import com.kerlann.healthsystem.CommonProxy;
+import com.kerlann.healthsystem.HealthSystem;
+import com.kerlann.healthsystem.client.gui.HealthHudOverlay;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+/**
+ * Proxy côté client pour gérer les initialisations spécifiques au client
+ */
 public class ClientProxy extends CommonProxy {
+    
+    private static final Logger LOGGER = LogManager.getLogger(HealthSystem.MODID);
+    
     @Override
     public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
-        // Enregistrer les rendus, etc.
+        LOGGER.info("Health System Client: Pre-initialization");
     }
 
     @Override
     public void init(FMLInitializationEvent event) {
         super.init(event);
-        // Initialisation côté client
+        LOGGER.info("Health System Client: Initialization");
+        
+        // Enregistrer le renderer de l'HUD
+        MinecraftForge.EVENT_BUS.register(HealthHudOverlay.class);
     }
 
     @Override
     public void postInit(FMLPostInitializationEvent event) {
         super.postInit(event);
-        // Post-initialisation côté client
+        LOGGER.info("Health System Client: Post-initialization");
     }
 }
